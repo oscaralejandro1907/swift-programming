a932b9eef14ed28ee0f4d47d70ef3161466d21b4
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-12-02 13:52:45 -0600

    Create SwiftTutorialApp.swift

---
+++ SwiftTutorial/SwiftTutorial/SwiftTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTutorialApp.swift
+//  SwiftTutorial
+//
+//  Created by Oscar Alejandro Hernández López on 02/12/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
