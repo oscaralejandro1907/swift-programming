de1225ead1bf61ca737538576c807e68fc8a20a9
Oscar Alejandro Hernandez Lopez <oscarale930719@gmail.com>
2020-12-02 13:52:37 -0600

    Create ContentView.swift

---
+++ SwiftTutorial/SwiftTutorial/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftTutorial
+//
+//  Created by Oscar Alejandro Hernández López on 02/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
